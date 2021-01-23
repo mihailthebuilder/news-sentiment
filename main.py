c6a66088884bdec75341d98e7097709bc2185cd8
Mihail Marian <mihailmarian12@gmail.com>
2021-01-23 14:22:45 +0000

    finished

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-""".."""
+"""Calculates how positive a website's content is. Scores usually range between -10 and +10"""
 import json
 import re
 import requests
@@ -10,7 +10,7 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 url = input("\nEnter the site you wish to analyse: ")
 
 # add https if not in there at start
-if url[0:9] != "https://":
+if url[0:8] != "https://":
     url = "https://" + url
 
 try:
@@ -19,20 +19,31 @@ try:
     # get the individual text pieces inside the web page as separate list elements
     soup_li = bs(req.text, "lxml").body.getText(separator="||").split("||")
 
+    # list which will hold the pieces of text together with their scores
     text_li = []
+
+    # Initialise the 2 sentiment analysis libraries used
     afinn = Afinn()
     analyzer = SentimentIntensityAnalyzer()
 
+    # sum of non-0 scores
     sum_text = 0
+
+    # count of non-0 scores
     count_text = 0
 
+    # max/min text scores holders
     max_score = 0
     max_text = ""
     min_score = 0
     min_text = ""
 
     for text in soup_li:
+
+        # only look at pieces of text with 5+ sentences
         if len(text.split()) >= 5:
+
+            # remove weird characters that sometimes appear
             text_lower = re.sub(r"\u2018|\u2019|\u2014", "", text.lower())
 
             afinn_score = afinn.score(text_lower)  # usually from -5 to +5
@@ -57,6 +68,7 @@ try:
                 if combined_score > max_score:
                     max_score = combined_score
                     max_text = text_lower
+
                 elif combined_score < min_score:
                     min_score = combined_score
                     min_text = text_lower
@@ -73,16 +85,23 @@ try:
     with open("output.json", "w") as file:
         json.dump(text_li, file)
 
-    print(
-        f"\n'{url}' has an average positivity score of {round(sum_text/count_text*2)}."
-    )
-    print(
-        f"\nThe most negative score was {min_score}, which came from this piece of text '{min_text}'"
-    )
-    print(
-        f"\nThe most positive score was {max_score}, which came from this piece of text '{max_text}'"
-    )
-    print(f"\n**Note: scores usually range between -10 and +10")
+    if count_text == 0:
+        print("\nUnable to calculate the scores.")
+
+    else:
+
+        print(
+            f"\n'{url}' has an average positivity score of {round(sum_text/count_text*2)}."
+        )
+        print(
+            f"\nThe most negative score was {min_score}, which came from this piece of text '{min_text}'"
+        )
+        print(
+            f"\nThe most positive score was {max_score}, which came from this piece of text '{max_text}'"
+        )
+        print("\n**Note: scores usually range between -10 and +10")
+
+    print("\nRaw data has been placed in the `output.json` file")
 
 # catch errors in requests.get statement
 except requests.exceptions.ConnectionError as error:
@@ -91,5 +110,5 @@ except requests.exceptions.ConnectionError as error:
     )
 except Exception as error:
     print(
-        f"Something went wrong after a successful request was made to the '{url}' URL.\n\nError message: '{error}'"
+        f"\nSomething went wrong after a successful request was made to the '{url}' URL.\n\nError message: '{error}'"
     )
